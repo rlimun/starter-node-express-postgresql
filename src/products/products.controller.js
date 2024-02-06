f4d87108b6ff048a3d41cb3c9c6ed534a2eef6d5
Richelle Limun <rlimun@indeed.com>
2024-02-05 21:16:01 -0800

    aggregates-with-knex-and-javascript

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -7,7 +7,7 @@ function read(req, res) {
   res.json({ data });
 }
 
-async function list(req, res, next) {
+async function list(req, res) {
   const data = await productsService.list();
   res.json({ data });
 }
@@ -21,7 +21,22 @@ async function productExists(req, res, next) {
   next({ status: 404, message: `Product cannot be found.` });
 }
 
+async function listOutOfStockCount(req, res, next) {
+  res.json({ data: await productsService.listOutOfStockCount() });
+}
+
+async function listPriceSummary(req, res, next) {
+  res.json({ data: await productsService.listPriceSummary() });
+}
+
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
+}
+
 module.exports = {
   read: [asyncErrorBoundary(productExists), read],
   list: asyncErrorBoundary(list),
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,16 +1,42 @@
 const knex = require("../db/connection");
 
-function list(req, res, next) {
-    productsService
-      .list()
-      .then((data) => res.json({ data }))
-      .catch(next);
-  }
+function listPriceSummary() {
+  return knex("products")
+    .select("supplier_id")
+    .min("product_price")
+    .max("product_price")
+    .avg("product_price")
+    .groupBy("supplier_id");
+}
 
 function read(productId) {
     return knex("products").select("*").where({ product_id: productId }).first();
   }
 
+function listOutOfStockCount() {
+    return knex("products")
+      .select("product_quantity_in_stock as out_of_stock")
+      .count("product_id")
+      .where({ product_quantity_in_stock: 0 })
+      .groupBy("out_of_stock");
+}
+
+function listTotalWeightByProduct() {
+  return knex("products")
+    .select(
+      "product_sku",
+      "product_title",
+      knex.raw(
+        "sum(product_weight_in_lbs * product_quantity_in_stock) as total_weight_in_lbs"
+      )
+    )
+    .groupBy("product_title", "product_sku");
+}
+
 module.exports = {
   list,
+  read,
+  listPriceSummary,
+  listOutOfStockCount,
+  listTotalWeightByProduct,
 };
